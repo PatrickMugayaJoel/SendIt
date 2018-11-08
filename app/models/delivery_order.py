28ad5c659e6a57f2c9402a07404d25e5b4824b77
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2018-11-08 16:06:24 +0300

    delivery order data structure

---
+++ app/models/delivery_order.py
@@ -0,0 +1,13 @@
+"""parcel delivery order model structure"""
+
+class DeliveryOrder:
+    """parcel delivery order model structure"""
+    def __init__(self, destination, previousCheckpoint, nextCheckpoint, pickupLocation, parcelSize, status, owner, date):
+        self.destination = destination
+        self.previousCheckpoint = previousCheckpoint
+        self.nextCheckpoint = nextCheckpoint
+        self.pickupLocation = pickupLocation
+        self.status = status
+        self.owner = owner
+        self.date = date
+        self.parcelSize = parcelSize
