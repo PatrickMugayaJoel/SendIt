b2fe1ca0db11bddcdf3d5e7092ef08f77f0a55d3
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2018-11-09 13:05:40 +0300

    get parcel order by userid

--- app/views/views.py
+++ app/views/views.py
@@ -47,4 +47,17 @@ def deliveryOrder(orderID):
         else:
             return 'Sorry parcel id: %d not found!'%orderID
     else:
-        return jsonify({'error': "bad request"}), 404        
+        return jsonify({'error': "bad request"}), 404
+
+#Get a parcels by userID
+@app.route('/api/v1/users/<int:userID>/parcels', methods=['GET'])
+def parcelOrders(userID):
+    if request.method == 'GET':
+        userparcel = list(filter(lambda parcel: parcel['owner'] == userID, parcelorders))
+        if len(userparcel):
+            return jsonify(userparcel)
+        else:
+            return 'Sorry userID id: %d not found!'%userID
+    else:
+        return jsonify({'error': "bad request"}), 404
+        
