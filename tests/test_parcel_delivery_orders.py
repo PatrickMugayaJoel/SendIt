c1815b7b465eedb852f1e783000e31a48531d702
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2018-11-15 06:57:54 +0300

    adding tests to cover the whole project and editing based on codeclimate

--- tests/test_parcel_delivery_orders.py
+++ tests/test_parcel_delivery_orders.py
@@ -18,7 +18,9 @@ class test_parcel_orders(unittest.TestCase):
         set up params
         """
         self.test = app.test_client()
-        self.parcel = {"orderID":"DO_1", "status":"in transit", "owner":2}
+        self.parcel = {"orderID":"DO_1", "status":"in transit", "userid":1}
+        self.user = {"userid":"DO_1", "username":"Josean", "password":"password", "role":"Admin"}
+        self.test.get('/users/cancel')
 
     def tearDown(self):
         """
@@ -41,7 +43,7 @@ class test_parcel_orders(unittest.TestCase):
         test parcels page
         """
         response = self.test.get('/api/v1/parcels', content_type='html/text')
-        self.assertEqual(response.status_code, 203)
+        self.assertEqual(response.status_code, 404)
 
     #test posting an empty parcel
     def test_post_empty_parcels(self):
@@ -49,7 +51,15 @@ class test_parcel_orders(unittest.TestCase):
         parcels page with empty list
         """
         response = self.test.post('/api/v1/parcels', content_type='html/text')
-        self.assertEqual(response.status_code, 203)
+        self.assertEqual(response.status_code, 404)
+
+    #test posting an empty user
+    def test_post_empty_user(self):
+        """
+        user page with empty list
+        """
+        response = self.test.post('/api/v1/users', content_type='html/text')
+        self.assertEqual(response.status_code, 404)
 
     #test get parcel by id
     def test_get_parcel_by_id(self):
@@ -57,7 +67,15 @@ class test_parcel_orders(unittest.TestCase):
         get a parcel by its id
         """
         response = self.test.get('/api/v1/parcels/3', content_type='html/text')
-        self.assertEqual(response.status_code, 203)
+        self.assertEqual(response.status_code, 404)
+
+    #test get user by id
+    def test_get_user_by_id(self):
+        """
+        get a user by id
+        """
+        response = self.test.get('/api/v1/users/3', content_type='html/text')
+        self.assertEqual(response.status_code, 404)
 
     #test get parcel by userid
     def test_get_parcel_by_userid(self):
@@ -65,16 +83,29 @@ class test_parcel_orders(unittest.TestCase):
         get parcels by user id
         """
         response = self.test.get('/api/v1/users/4/parcels', content_type='html/text')
-        self.assertEqual(response.status_code, 203)
+        self.assertEqual(response.status_code, 404)
 
     #test posting with parcel data
     def test_post_data_parcels(self):
         """
         posting parcel data
         """
+        
+        self.test.post("/api/v1/users", headers={"Content-Type": "application/json"}, data=json.dumps(self.user))
         res = self.test.post("/api/v1/parcels", headers={"Content-Type": "application/json"}, data=json.dumps(self.parcel))
         res_data = json.loads(res.data)
-        expected_output = {"orderID":1, "status":"in transit", "owner":2}
+        expected_output = {"orderID":1, "status":"in transit", "userid":1}
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(res_data, expected_output)
+
+    #test posting user data
+    def test_post_data_users(self):
+        """
+        posting user data
+        """
+        res = self.test.post("/api/v1/users", headers={"Content-Type": "application/json"}, data=json.dumps(self.user))
+        res_data = json.loads(res.data)
+        expected_output = {"userid":1, "username":"Josean", "password":"password", "role":"Admin"}
         self.assertEqual(res.status_code, 201)
         self.assertEqual(res_data, expected_output)
 
@@ -84,4 +115,4 @@ class test_parcel_orders(unittest.TestCase):
         cancel a parcel
         """
         response = self.test.put('/api/v1/parcels/3/cancel', content_type='html/text')
-        self.assertEqual(response.status_code, 203)
+        self.assertEqual(response.status_code, 404)
