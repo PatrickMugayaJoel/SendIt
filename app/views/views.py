1b6e1762906c99ec3088439b939f612821cfbcbe
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2018-11-15 07:01:06 +0300

    implementing user model. as advised by LFA

--- app/views/views.py
+++ app/views/views.py
@@ -6,6 +6,7 @@ from flask import jsonify, request
 from app import app
 from app.utils.controllers import create_id
 parcelorders = []
+users = [{"userid":1, "username":"Josean", "password":"password", "role":"Admin"}]
 
 #index route
 @app.route('/')
@@ -15,32 +16,33 @@ def home():
     """
     return "Welcome to SendIT.", 200
 
-#get all delivery orders & post a delivery order
-@app.route('/api/v1/parcels', methods=['GET', 'POST'])
+#get all delivery orders
+@app.route('/api/v1/parcels', methods=['GET'])
 def deliveryOrders():
     """
-    parcels route
+    get parcels route
     """
-    if request.method == 'GET':
+    if parcelorders:
+        return jsonify(parcelorders), 200
+    return jsonify({"message":"There are no orders to display"}), 404
 
-        if parcelorders:
-            return jsonify(parcelorders), 200
-        return jsonify({"message":"There are no orders to display"}), 203
-    if request.method == 'POST':
-
-        data = request.get_json()
-
-        if data:
+#post a delivery order
+@app.route('/api/v1/parcels', methods=['POST'])
+def deliveryOrderspost():
+    """
+    post parcels route
+    """
+    data = request.get_json()
+    if data:
+        user = [item["userid"] for item in users if item["userid"] == data["userid"]]
+        if user[0]:
             #generate an id
             orderID = create_id(parcelorders)
-
             data['orderID'] = orderID
-
             # appends the delivery orders object to list
             parcelorders.append(data)
             return jsonify(data), 201
-        return jsonify({"message":"No data was posted"}), 203
-    return jsonify({'error': "bad request"}), 400
+    return jsonify({"message":"User id was not found or No data was posted"}), 404
 
 #Get a parcel by ID
 @app.route('/api/v1/parcels/<int:orderID>', methods=['GET'])
@@ -48,12 +50,10 @@ def deliveryOrder(orderID):
     """
     selecting a parcel by id
     """
-    if request.method == 'GET':
-        parcel = [item for item in parcelorders if item["orderID"] == orderID]
-        if parcel:
-            return jsonify(parcel), 200
-        return 'Sorry parcel id: %d not found!'%orderID, 203
-    return jsonify({'error': "bad request"}), 400
+    parcel = [item for item in parcelorders if item["orderID"] == orderID]
+    if parcel:
+        return jsonify(parcel), 200
+    return 'Sorry parcel with id: %d not found!'%orderID, 404
 
 #Get a parcels by userID
 @app.route('/api/v1/users/<int:userID>/parcels', methods=['GET'])
@@ -61,12 +61,10 @@ def parcelOrders(userID):
     """
     selscting parcel by userid
     """
-    if request.method == 'GET':
-        userparcel = list(filter(lambda parcel: parcel['owner'] == userID, parcelorders))
-        if userparcel:
-            return jsonify(userparcel), 200
-        return 'Sorry userID id: %d not found!'%userID, 203
-    return jsonify({'error': "bad request"}), 400
+    userparcel = list(filter(lambda parcel: parcel['userid'] == userID, parcelorders))
+    if userparcel:
+        return jsonify(userparcel), 200
+    return 'Sorry user with id: %d not found!'%userID, 404
 
 #Cancel a parcel delivery order
 @app.route('/api/v1/parcels/<int:orderID>/cancel', methods=['PUT'])
@@ -74,22 +72,65 @@ def parcelOrder(orderID):
     """
     canceling a parcel
     """
-    if request.method == 'PUT':
-        if parcelorders:
-            parcel = [item for item in parcelorders if item["orderID"] == orderID]
-            if parcel:
-                parcel = parcel[0]
-                parcel['status'] = 'Canceled'
-                return jsonify(parcel), 200
-            return 'Sorry parcel order id: %d not found!'%orderID, 203
-        return 'Sorry No parcel orders found!', 203
-    return jsonify({'error': "bad request"}), 400
+    if parcelorders:
+        parcel = [item for item in parcelorders if item["orderID"] == orderID]
+        if parcel:
+            parcel = parcel[0]
+            parcel['status'] = 'Canceled'
+            return jsonify(parcel), 200
+    return 'Sorry parcel order id: %d not found!'%orderID, 404
 
-#Clear parcels in memory
+#post. create a user
+@app.route('/api/v1/users', methods=['POST'])
+def createuserpost():
+    """
+    post. create users route
+    """
+    data = request.get_json()
+    if data:
+        #generate an id
+        userid = create_id(users)
+        data['userid'] = userid
+        # appends user object to list
+        users.append(data)
+        return jsonify(data), 201
+    return jsonify({"message":"No data was posted"}), 404
+
+#get all users
+@app.route('/api/v1/users', methods=['GET'])
+def getusers():
+    """
+    get users route
+    """
+    if users:
+        return jsonify(users), 200
+    return jsonify({"message":"There are no users to display"}), 404
+
+#Get a user by ID
+@app.route('/api/v1/users/<int:userid>', methods=['GET'])
+def getuserbyid(userid):
+    """
+    get a user by id
+    """
+    user = [item for item in users if item["userid"] == userid]
+    if user:
+        return jsonify(user), 200
+    return 'Sorry user with id: %d not found!'%userid, 404
+
+#Clear all parcels
 @app.route('/parcels/cancel', methods=['GET'])
 def cancelparcels():
     """
-    canceling a parcel
+    canceling all parcels
     """
     del parcelorders[:]
+    return 'No result',204
+
+#Clear all users
+@app.route('/users/cancel', methods=['GET'])
+def cancelusers():
+    """
+    canceling all users
+    """
+    del users[:]
     return 'No result',204
