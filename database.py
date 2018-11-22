6f9e4e3de0f10c2cad69f22cbd8810d69246f009
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2018-11-22 14:09:26 +0300

    Update database.py

--- database.py
+++ database.py
@@ -13,11 +13,11 @@ class DatabaseConnection:
     def __init__(self):
 
         try:
-            self.conn = psycopg2.connect(host="ec2-50-19-249-121.compute-1.amazonaws.com",
-                                            database="d9fsit8u8si0p2",
-                                            user="tzhzehaqthsqlr",
-                                            password="6c87b77ee20e8d9c7b84f962daaace4de1736bebe09481b178032f6bddc24b67",
-                                            port="5432")
+            self.conn = psycopg2.connect(host=os.environ.get('host'),
+                                            database=os.environ.get('database'),
+                                            user=os.environ.get('user'),
+                                            password=os.environ.get('password'),
+                                            port=os.environ.get('port'))
                                         
             self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
             self.conn.autocommit = True
