bc89f78f11ef555e68d0ec8ec57113603af5900e
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2018-12-20 08:17:53 +0300

    adding documentation

--- app/views/views.py
+++ app/views/views.py
@@ -6,7 +6,7 @@ from app.utilities.utils import serialize
 from app.models.delivery_order import DeliveryOrder
 import datetime
 from database import DatabaseConnection
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 from flask.views import MethodView
 from flask_jwt_extended import ( JWTManager, jwt_required, create_access_token, get_jwt_identity, get_raw_jwt)
 
@@ -31,17 +31,16 @@ def check_if_token_in_blacklist(decrypted_token):
 @app.route('/')
 def home():
     """ home route """
-    return jsonify({"message":'welcome',"status":"success"}), 200
+    # return jsonify({"message":'welcome',"status":"success"}), 200
+    return render_template("index.html")
 
 class SignupView(MethodView):
-    """users description"""
-    #post. Signup a user
-    #@app.route('/api/v1/signup', methods=['POST'])
-    def Signup(self):
-        """ post. create users route """
+    """user signup"""
+
+    def post(self):
+        """ signup """
         data = request.get_json()
         if data:
-
             newuser = User()
             thisuser = newuser.add(data)
             if not thisuser == True:
@@ -58,10 +57,10 @@ class SignupView(MethodView):
         return jsonify({"message":"No data was posted","status":"failed"}), 400
 
 class LoginView(MethodView):
-    #Login route
-    #@app.route('/api/v1/login', methods=['POST'])
-    def Login(self):
-        """login route"""
+    """ Login """
+
+    def post(self):
+        """login"""
         if not request.is_json:
             return jsonify({"message":"Missing JSON data","status":"failed"}), 400
 
@@ -77,12 +76,25 @@ class LoginView(MethodView):
         return jsonify({'access_token':access_token, 'status':'Successfull'}), 200    
 
 class UserView(MethodView):
-    #get all users
-    #@app.route('/api/v1/users', methods=['GET'])
+    """ user methods """
+
     @jwt_required
-    def Listusers(self):
-        """ get users route """
-            
+    def get(self, userid=None):
+        """get all users, get a user by ID """
+
+        """ get a user by ID """
+        if userid:
+            if not get_jwt_identity()['role'] == 'admin':
+                return jsonify({"message":'Request denied. You have to be an administrator!',"satus":"failed"}), 401
+            user = database.getoneUser(userid)
+            try:
+                if user['userid']:
+                    return jsonify({"user":user,"satus":"success"}), 200
+                return user['message'], 400
+            except:
+                    return jsonify({"message":"User does not exist","satus":"failed"}), 400
+
+        """get all users """
         if not get_jwt_identity()['role'] == 'admin':
             return jsonify({"message":'Request denied. You have to be an administrator!',"satus":"failed"}), 401
         listusers = database.getUsers()
@@ -90,27 +102,9 @@ class UserView(MethodView):
             return jsonify({"users":listusers,"status":"success"}), 200
         return jsonify({"message":"There are no users to display","satus":"failed"}), 400
 
-    #Get a user by ID
-    #@app.route('/api/v1/users/<int:userid>', methods=['GET'])
     @jwt_required
-    def Getuserbyid(self, userid):
-        """ get a user by id """
-            
-        if not get_jwt_identity()['role'] == 'admin':
-            return jsonify({"message":'Request denied. You have to be an administrator!',"satus":"failed"}), 401
-        user = database.getoneUser(userid)
-        try:
-            if user['userid']:
-                return jsonify({"user":user,"satus":"success"}), 200
-            return user['message'], 400
-        except:
-                return jsonify({"message":"User does not exist","satus":"failed"}), 400
-
-    #Promote user
-    #@app.route('/api/v1/users/<int:userid>/promote', methods=['PUT'])
-    @jwt_required
-    def Promote(self, userid):
-        """ get a user by id """
+    def put(self, userid):
+        """ Promote a user """
             
         if not get_jwt_identity()['role'] == 'admin':
             return jsonify({"message":'Request denied. You have to be an administrator!',"satus":"failed"}), 401
@@ -126,9 +120,9 @@ class UserView(MethodView):
 
 class LogoutView(MethodView):
     """Logout"""
-    #@app.route('/api/v1/logout', methods=['DELETE'])
+
     @jwt_required
-    def Logout(self):
+    def delete(self):
         """ logout """
         jti = get_raw_jwt()['jti']
         blacklist.add(jti)
@@ -138,84 +132,66 @@ class LogoutView(MethodView):
 
 class ParcelView(MethodView):
     #post a delivery order
-    #@app.route('/api/v1/parcels', methods=['POST','GET'])
     @jwt_required
-    def ParcelDeliveryOrder(self):
-        """ post parcels route """
+    def post(self):
+        """ post a parcel """
 
-        if request.method == 'POST':
-            userdata = get_jwt_identity()
-            if userdata["role"]=="admin":
-                return jsonify({"message":"Action not allowed for Admins","status":"failed"}), 401
-                
-            data = request.get_json()
-            if data:
-                data['userid'] = userdata['userid']
-                newparcel = DeliveryOrder()
-                result = newparcel.add(data)
-                if not result == True:
-                    return jsonify(result), 400
-                newparcel = serialize(newparcel)
+        userdata = get_jwt_identity()
+        if userdata["role"]=="admin":
+            return jsonify({"message":"Action not allowed for Admins","status":"failed"}), 401
+            
+        data = request.get_json()
+        if data:
+            data['userid'] = userdata['userid']
+            newparcel = DeliveryOrder()
+            result = newparcel.add(data)
+            if not result == True:
+                return jsonify(result), 400
+            newparcel = serialize(newparcel)
 
-                if not database.insert_data_parcels(newparcel)==True:
-                    return jsonify({"message":"Parcel not saved","status":"failed"}),400
+            if not database.insert_data_parcels(newparcel)==True:
+                return jsonify({"message":"Parcel not saved","status":"failed"}),400
 
-                return jsonify({"parcel":newparcel,"status":"success"}), 201
-            return jsonify({"message":"No data was posted","status":"failed"}), 400
+            return jsonify({"parcel":newparcel,"status":"success"}), 201
+        return jsonify({"message":"No data was posted","status":"failed"}), 400
+
+    @jwt_required
+    def get(self, orderID = None, userID = None):
+        """ get a parcel, get all parcels ,get parcels by userid """
         
-        elif request.method == 'GET':
+        userdata=get_jwt_identity()
 
-            userdata=get_jwt_identity()
-            if userdata['role'] == 'admin':
-                myparcelorders = database.getparcels()
-            else:
-                myparcelorders = database.getparcelsbyuser(userdata['userid'])
-            if myparcelorders:
-                return jsonify({"Parcels":myparcelorders,"status":"success"}), 200
-            return jsonify({"message":"No parcel orders to display","status":"failed"}), 400
-        
+        """ get a parcel """
+        if orderID:
+            parcel = database.getoneparcel(orderID)
+            if parcel:
+                return jsonify({"parcels":parcel,"status":"success"}), 200
+            return jsonify({"message":'Sorry parcel not found!',"status":"failed"}), 404
+
+        if userID:
+            """ get parcels by userid """
+            userparcel = database.getparcelsbyuser(userID)
+            if userparcel:
+                return jsonify(userparcel), 200
+            return jsonify({"message":"Not found!","status":"failed"}), 404
+
+        """selecting all parcels"""
+        if userdata['role'] == 'admin':
+            myparcelorders = database.getparcels()
         else:
-            return jsonify({'message': "bad request","status":"failed"}), 404
+            myparcelorders = database.getparcelsbyuser(userdata['userid'])
+        if myparcelorders:
+            return jsonify({"Parcels":myparcelorders,"status":"success"}), 200
+        return jsonify({"message":"No parcel orders to display","status":"failed"}), 400
 
-    #Get a parcels by userID
-    #@app.route('/api/v1/users/<int:userID>/parcels', methods=['GET'])
     @jwt_required
-    def UserOrders(self, userID):
-        """ selscting parcel by userid """
-            
-        userparcel = database.getparcelsbyuser(userID)
-        if userparcel:
-            return jsonify(userparcel), 200
-        return jsonify({"message":"Not found!","status":"failed"}), 404
-
-    #Cancel a parcel delivery order
-    #@app.route('/api/v1/parcels/<int:orderID>/cancel', methods=['PUT'])
-    @jwt_required
-    def CancelOrder(self, orderID):
-        """ canceling a parcel """
-            
-        parcel = database.getoneparcel(orderID)
-        if parcel:
-            userdata = get_jwt_identity()
-            if not parcel['userid'] == userdata['userid']:
-                return jsonify({"message":"Update Rights denied!","status":"failed"}), 401
-            parcel['status'] = 'Cancelled'
-            database.update_parcel(orderID,parcel)
-            return jsonify({"parcel":parcel,"status":"success"}), 200
-        return jsonify({"message":"Parcel Not found!","status":"failed"}), 404
-
-    #Update a parcel delivery order
-    #@app.route('/api/v1/parcels/<int:orderID>/update', methods=['PUT'])
-    @jwt_required
-    def UpdateOrder(self, orderID):
+    def put(self, orderID):
         """ updating a parcel """
 
         identity = get_jwt_identity()
         parcel = database.getoneparcel(orderID)
 
-        if parcel:
-            pass
-        else:
+        if not parcel:
             return jsonify({"message":"Parcel not found","status":"failed"}), 404
 
         if parcel["userid"]==identity["userid"] or identity["role"]=="admin":
@@ -237,16 +213,19 @@ class ParcelView(MethodView):
             return jsonify({'message':"Parcel successfuly Updated", "status":"success"}), 200
         return jsonify({'message':dbresult, "status":"failed"}), 400
 
-    #Get a parcel by ID
-    #@app.route('/api/v1/parcels/<int:orderID>', methods=['GET'])
     @jwt_required
-    def ParcelOrder(self, orderID):
-        """ selecting a parcel by id """
+    def delete(self, orderID):
+        """ canceling a parcel """
             
         parcel = database.getoneparcel(orderID)
         if parcel:
-            return jsonify({"parcels":parcel,"status":"success"}), 200
-        return jsonify({"message":'Sorry parcel not found!',"status":"failed"}), 404
+            userdata = get_jwt_identity()
+            if not parcel['userid'] == userdata['userid']:
+                return jsonify({"message":"Update Rights denied!","status":"failed"}), 401
+            parcel['status'] = 'Cancelled'
+            database.update_parcel(orderID,parcel)
+            return jsonify({"parcel":parcel,"status":"success"}), 200
+        return jsonify({"message":"Parcel Not found!","status":"failed"}), 404
 
 app.add_url_rule('/api/v1/logout', view_func= LogoutView.as_view('Logout'),
                  methods=["DELETE"])
@@ -265,8 +244,8 @@ app.add_url_rule('/api/v1/parcels', view_func= ParcelView.as_view('ParcelDeliver
 app.add_url_rule('/api/v1/users/<int:userID>/parcels', view_func= ParcelView.as_view('UserOrders'),
                  methods=["GET"])
 app.add_url_rule('/api/v1/parcels/<int:orderID>/cancel', view_func= ParcelView.as_view('CancelOrder'),
-                 methods=["PUT"])
-app.add_url_rule('//api/v1/parcels/<int:orderID>/update', view_func= ParcelView.as_view('UpdateOrder'),
+                 methods=["DELETE"])
+app.add_url_rule('/api/v1/parcels/<int:orderID>/update', view_func= ParcelView.as_view('UpdateOrder'),
                  methods=["PUT"])
 app.add_url_rule('/api/v1/parcels/<int:orderID>', view_func= ParcelView.as_view('ParcelOrder'),
                  methods=["GET"])
